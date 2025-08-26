9ee623b290ecd901192008ad759bef43a48be7f0
Luis Starlino <luis.guilherme009@gmail.com>
2025-08-26 07:40:34 -0300

    feat(puter): add the scripts and types to use this library

---
+++ app/routes/auth.tsx
@@ -0,0 +1,21 @@
+/*****************************************************************************************
+* @Author: Luis Starlino
+* @Date: 2025-08-26 07:40
+*****************************************************************************************/
+import React from 'react'
+
+// ===== METADATA =====
+export const meta = () => ([
+    {title: 'Vaga Certa | Login'},
+    {name: 'description', content: 'Entre com sua conta'}
+])
+
+const auth = () => {
+  return (
+    <main className={"bg-[url('/images/bg-main.svg')] bg-cover"}>
+        
+    </main>
+  )
+}
+
+export default auth
