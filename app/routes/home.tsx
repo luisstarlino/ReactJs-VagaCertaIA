c15232c99144e8dd63802fa059cf6bcbee716887
Luis Starlino <luis.guilherme009@gmail.com>
2025-08-26 06:48:22 -0300

    feat(navbar): finish the first setup and configuration for this component

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,5 +1,11 @@
+/*****************************************************************************************
+* @Author: Luis Starlino
+* @Date: 2025-08-25 20:39
+*****************************************************************************************/
+import { resumes } from "contants";
 import Navbar from "~/components/Navbar";
 import type { Route } from "./+types/home";
+import ResumeCard from "~/components/ResumeCard";
 
 export function meta({ }: Route.MetaArgs) {
   return [
@@ -15,10 +21,22 @@ export default function Home() {
     <Navbar />
 
     <section className={"main-section"}>
-      <div className={"page-heading"}>
+
+      {/* TITLE */}
+      <div className={"page-heading py-16"}>
         <h1>Melhore suas aplicações e seu Currículo!</h1>
         <h2>Use a IA para melhorar sua apresentação e encontre o emprego dos sonhos.</h2>
       </div>
+
+      {/* RESUMES ANIMATION BACKGROUND */}
+      {resumes.length > 0 && (
+        <div className="resumes-section">
+          {resumes.map((resume) => (
+            <ResumeCard key={resume.id} resume={resume} />
+          ))}
+        </div>
+      )}
     </section>
+
   </main>;
 }
